3ce642d1ce62c32c18a92e0119ff303a9ce87683
shereenbajaj <shereen.bajaj@metago.health>
2026-09-23 19:33:29 +0530

    Delete supabase_setup.sql

--- supabase_setup.sql
+++
@@ -1,5 +0,0 @@
--- Already applied to project njgctrmitailbvjtyeiz as migration "otd_dashboard_readonly_api".
--- Kept here for reference. To allow another email domain:
-insert into private.otd_dash_allowed_domains(domain) values ('ketto.org') on conflict do nothing;
--- To see who's allowed:
-select * from private.otd_dash_allowed_domains;
